73b3a9e43f2ef259ea3355b6c1421cb7fd11b66c
evileye290-bit <evileye290@gmail.com>
2026-03-07 23:59:42 +0700

    update launcher

--- obj/Debug/net8.0-windows/C9Launcher.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/C9Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C9Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e269bc1e90e884407bb926c5c07b19ef7e99a244")]
 [assembly: System.Reflection.AssemblyProductAttribute("C9Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C9Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -180,7 +180,7 @@ namespace C9Launcher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/C9Launcher;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/C9Launcher;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
